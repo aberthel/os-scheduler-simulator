1ddb41359e286348c30797c9da7d8ac5df995290
aberthel <32374732+aberthel@users.noreply.github.com>
2018-11-29 17:21:26 -0500

    dynamic allocation, bug fix
    changed process list from statically allocated to dynamically allocated
    fixed time over bug in fifo implementation

--- TBD.c
+++ TBD.c
@@ -4,6 +4,7 @@
 #include<ctype.h>
 #include<unistd.h>
 
+/* process structures store crucial information about each process */
 struct process {
   int id;
   int enter_time;
@@ -20,243 +21,115 @@ struct process {
 int io_time = 10;
 
 //things to be specified
-int num_processes;
-
+int num_processes = 10;
+/* PARAMETER DEFAULTS */  
+int min_CPU_time = 10;
+int max_CPU_time = 100;
+int min_enter_time = 0;
+int max_enter_time = 100;
+int io_freq = 50; // ~50% of the time should be spent in io
+	
 //methods
-void print_process_status(struct process p[], int np);
+void print_process_status(struct process *p[], int np);
 int isInArray(int x, int* arr, int until);
 void check_digit(char* s);
-void fifo(struct process p[]);
-int estimate_time(struct process p[]);
+void fifo(struct process *p[]);
+int estimate_time(struct process *p[]);
 int comp_times(const void *a, const void *b);
-int is_io_time(struct process x);
-int processes_completed(struct process p[]);
+int is_io_time(struct process *x);
+int processes_completed(struct process *p[]);
 void print_array_to_file(int* array[], int l, char* file_name);
+void check_arg(int t);
+void gen_process(struct process *proc, int i);
+void read_process(struct process *proc[], FILE *file);
+void selection_sort(struct process *p[]);
+int fifo_process(struct process *p[], int* results_array[], int pc, int time_start, int time_estimate);
 
+/* main method */
 int main(int argc, char* argv[]) {
-  num_processes = 10;
-
-  int min_CPU_time = 10;
-  int max_CPU_time = 100;
-
-  int min_enter_time = 0;
-  int max_enter_time = 100;
-
-  int io_freq = 50; // 50% of the time should be spent in io
-
-  /* SET PROCESS PARAMETERS */
 
 
-  //TODO handling for parsing text files
-
+	/* SET PROCESS PARAMETERS */
 	FILE *file;
   
-  int counter = 0;
-  int process_file = 0; //0 if no process list has been specified, 1 otherwise
+  	int counter = 0; //keeps track of the arguments that have been processed
+  	int process_file = 0; //0 if no process file has been specified, 1 otherwise
 
   
   //loop through arguments to parse them
-  while(counter < argc) {
+  	while(counter < argc) {
     
-    char* arg = argv[counter];
+    	// get argument
+    	char* arg = argv[counter];
     
-    if(strcmp(arg, "-np")==0) {
-	counter++;
-     	int temp = atoi(argv[counter]);
-		if(temp == 0) {
-	  		printf("Error: invalid argument input\n");
-	  		exit(1);
-	  	}
-		num_processes = temp;
-    } else if(strcmp(arg, "-minCPU") == 0) {
-        counter++;
-     	int temp = atoi(argv[counter]);
-
-		if(temp <= 0) {
-	  		printf("Error: invalid argument input\n");
-	  		exit(1);
-	  	}
-
-		min_CPU_time = temp;
-    } else if(strcmp(arg, "-maxCPU") == 0) {
-        counter++;
-     	int temp = atoi(argv[counter]);
-
-		if(temp <= 0) {
-	  		printf("Error: invalid argument input\n");
-	  		exit(1);
-	  	}
-
-		max_CPU_time = temp;
-    } else if(strcmp(arg, "-mine") == 0) {
-        counter++;
-	
-		if(isdigit(argv[counter][0]) ==0) {
-	  		printf("Error: invalid argument input\n");
-	  		exit(1);
-		}
-	
-     	int temp = atoi(argv[counter]);
-
-		min_enter_time = temp;
-    } else if(strcmp(arg, "-maxe") == 0) {
-        counter++;
-
-		if(isdigit(argv[counter][0]) ==0) {
-	  		printf("Error: invalid argument input\n");
-	  		exit(1);
-		}
-	
-     	int temp = atoi(argv[counter]);
-
-		max_enter_time = temp;
-    } else if(strcmp(arg, "-io") == 0) {
-        counter++;
-
-		if(isdigit(argv[counter][0]) ==0) {
-	  		printf("Error: invalid argument input\n");
-	  		exit(1);
-		}
-	
-     	int temp = atoi(argv[counter]);
-
-		io_freq = temp;
-    } else if(access(arg, R_OK) == 0 && counter != 0) {
-    	file = fopen(arg, "r");
-    	process_file = 1;
+    	if(strcmp(arg, "-np")==0) {
+			counter++;
+     		int temp = atoi(argv[counter]);
+			check_arg(temp);
+			num_processes = temp;
+    	} else if(strcmp(arg, "-minCPU") == 0) {
+        	//first three commands here could be collapsed into one...
+        	counter++;
+     		int temp = atoi(argv[counter]);
+			check_arg(temp);
+			min_CPU_time = temp;
+    	} else if(strcmp(arg, "-maxCPU") == 0) {
+        	counter++;
+     		int temp = atoi(argv[counter]);
+			check_arg(temp);
+			max_CPU_time = temp;
+    	} else if(strcmp(arg, "-mine") == 0) {
+        	counter++;
+			check_digit(argv[counter]);
+     		int temp = atoi(argv[counter]);
+			min_enter_time = temp;
+    	} else if(strcmp(arg, "-maxe") == 0) {
+        	counter++;
+			check_digit(argv[counter]);
+     		int temp = atoi(argv[counter]);
+			max_enter_time = temp;
+    	} else if(strcmp(arg, "-io") == 0) {
+        	counter++;
+			check_digit(argv[counter]);
+     		int temp = atoi(argv[counter]);
+			io_freq = temp;
+    	} else if(access(arg, R_OK) == 0 && counter != 0) { //filepath with process file
+    		file = fopen(arg, "r");
+    		process_file = 1;
     	
-    	//First line of file should be "process_num: %d" where %d is the number of processes described
-    	char buffer[255];
-    	fgets(buffer, 255, file);
+    		//First line of file should be "process_num: %d" where %d is the number of processes described
+    		char buffer[255];
+    		fgets(buffer, 255, file);
     	
-    	char *string;
-    	string = strtok(buffer, " ");
+    		char *string;
+    		string = strtok(buffer, " ");
     	
-    	if(strcmp(string, "process_num:") != 0) {
-    		printf("Error: improperly formatted process file \n");
-    		exit(1);
+    		if(strcmp(string, "process_num:") != 0) {
+    			printf("Error: improperly formatted process file \n");
+    			exit(1);
+    		}
+    	
+    		string = strtok(NULL, " \n");
+    		check_digit(string);
+    	
+    		int temp = atoi(string);
+    		num_processes = temp;
     	}
-    	
-    	string = strtok(NULL, " \n");
-    	
-    	if(isdigit(string[0]) == 0) {
-    		printf("Error: improperly formatted process file \n");
-    		exit(1);
-    	}
-    	
-    	int temp = atoi(string);
-    	num_processes = temp;
-    }
+    	counter++;
+  	}
 
 
-    counter++;
-  }
-
-
-
-  
   //create processes based off of the list given
-  struct process p[num_processes];
+  struct process *p[num_processes];
 
 	//no process file has been specified: randomly generate processes
 	if(process_file == 0) {
   		for(int i=0; i<num_processes; i++) {
-  			p[i].id = i;
-  			p[i].status = 0;
-  			p[i].io_timer = 0;
-    		if(max_enter_time == min_enter_time) {
-      			p[i].enter_time = max_enter_time;
-    		} else {
-      			p[i].enter_time = (rand()%(max_enter_time-min_enter_time))+ min_enter_time;
-    		}
-
-    		if(max_CPU_time == min_CPU_time) {
-   	   			p[i].CPU_time = min_CPU_time;
-    		} else {
-      			p[i].CPU_time = (rand()%(max_CPU_time-min_CPU_time))+ min_CPU_time;
-    		}
-
-
-    		p[i].time_counter = 0;
-    		p[i].priority = 50;
-
-    		int num_io = p[i].CPU_time/io_time;
-    		num_io = num_io*io_freq/100;
-
-    		p[i].num_io = num_io;
-    
-    		int* pointer = malloc(sizeof(int[num_io]));
-
-    		for(int j=0; j<num_io; j++) {
-    			int x = (rand()%p[i].CPU_time);
-    			while(isInArray(x, pointer, j-1) == 0) {
-    				x = (rand()%p[i].CPU_time);
-    			}
-      			pointer[j] = x;
-    		}
-
-    		p[i].io_times = pointer;
+    		p[i] = malloc(sizeof(struct process));
+    		gen_process(p[i], i);
   		}
   	} else { //processes have been specified by file
-  		char buffer[255];
-  		char *string;
-  		
-  		fgets(buffer, 255, file);
-  		string = strtok(buffer, " ");
-  		
-  		for(int i=0; i<num_processes; i++) {
-  			//get to next process description
-  			while(strcmp(string, "Process") != 0) {
-  				fgets(buffer, 255, file);
-  				string = strtok(buffer, " ");
-  			}
-  			
-			p[i].id = i;
-  			
-  			for(int j=0; j<4; j++) {
-  				fgets(buffer, 255, file);
-  				string = strtok(buffer, " ");
-  				string = strtok(NULL, " \n");
-  				check_digit(string);
-  				int temp = atoi(string);
-  				
-  				switch(j) {
-  					case 0: 
-  						p[i].enter_time = temp;
-  						break;
-  					case 1:
-  						p[i].CPU_time = temp;
-  						break;
-  					case 2:
-  						p[i].priority = temp;
-  						break;
-  					case 3:
-  						p[i].num_io = temp;
-  						break;
-  				}
-  			}
-  			
-  			//get line with IO numbers on it
-  			fgets(buffer, 255, file);
-  			string = strtok(buffer, " ");
-  			
-  			int* pointer = malloc(sizeof(int[p[i].num_io]));
-  			
-  			for(int j=0; j<p[i].num_io; j++) {
-    			string = strtok(NULL, " \n");
-    			check_digit(string); 			
-    			int x = atoi(string);
-      			pointer[j] = x;
-    		}
-
-    		p[i].io_times = pointer;
-    		
-    		p[i].time_counter = 0;
-    		p[i].status = 0;
-    		p[i].io_timer = 0;
-
-  		}
-  		fclose(file);
+  		read_process(p, file);
   	}
   	
   	file = NULL;
@@ -281,13 +154,158 @@ void check_digit(char* s) {
 	}
 }
 
+void check_arg(int t) {
+	if(t <= 0) {
+	  	printf("Error: invalid argument input\n");
+	  	exit(1);
+	}
+}
+
+
+void read_process(struct process *proc[], FILE *file) {
+	char buffer[255];
+  	char *string;
+  		
+  	fgets(buffer, 255, file);
+  	string = strtok(buffer, " ");
+  		
+  	for(int i=0; i<num_processes; i++) {
+  	
+  		proc[i] = malloc(sizeof(struct process));
+  		
+  		//get to next process description
+  		while(strcmp(string, "Process") != 0) {
+  			fgets(buffer, 255, file);
+  			string = strtok(buffer, " ");
+  		}
+  			
+		proc[i]->id = i;
+  			
+  		for(int j=0; j<4; j++) {
+  			fgets(buffer, 255, file);
+  			string = strtok(buffer, " ");
+  			string = strtok(NULL, " \n");
+  			check_digit(string);
+  			int temp = atoi(string);
+  				
+  			switch(j) {
+  				case 0: 
+  					proc[i]->enter_time = temp;
+  					break;
+  				case 1:
+  					proc[i]->CPU_time = temp;
+  					break;
+  				case 2:
+  					proc[i]->priority = temp;
+  					break;
+  				case 3:
+  					proc[i]->num_io = temp;
+  					break;
+  			}
+  		}
+  			
+  		//get line with IO numbers on it
+  		fgets(buffer, 255, file);
+  		string = strtok(buffer, " ");
+  			
+  		int* pointer = malloc(sizeof(int[proc[i]->num_io]));
+  			
+  			for(int j=0; j<proc[i]->num_io; j++) {
+    		string = strtok(NULL, " \n");
+    		check_digit(string); 			
+    		int x = atoi(string);
+      		pointer[j] = x;
+    	}
+
+    	proc[i]->io_times = pointer;
+    		
+    	proc[i]->time_counter = 0;
+    	proc[i]->status = 0;
+    	proc[i]->io_timer = 0;
+
+  	} 
+  	fclose(file); 
+}
+
+
+void gen_process(struct process *proc, int i) {
+	proc->id = i;
+  	proc->status = 0;
+  	proc->io_timer = 0;
+  	
+  	
+    if(max_enter_time == min_enter_time) {
+      	proc->enter_time = max_enter_time;
+    } else {
+      	proc->enter_time = (rand()%(max_enter_time-min_enter_time))+ min_enter_time;
+    }
+
+    if(max_CPU_time == min_CPU_time) {
+   	   	proc->CPU_time = min_CPU_time;
+    } else {
+      	proc->CPU_time = (rand()%(max_CPU_time-min_CPU_time))+ min_CPU_time;
+    }
+
+	
+    proc->time_counter = 0;
+    proc->priority = 50;
+	
+    int num_io = proc->CPU_time/io_time;
+    num_io = num_io*io_freq/100;
+
+    proc->num_io = num_io;
+    
+    int* pointer = malloc(sizeof(int[num_io]));
+
+    for(int j=0; j<num_io; j++) {
+    	int x = (rand()%proc->CPU_time);
+    	while(isInArray(x, pointer, j-1) == 0) {
+    		x = (rand()%proc->CPU_time);
+    	}
+      	pointer[j] = x;
+    }
+
+    proc->io_times = pointer;
+    
+
+}
+
+
+//sorts the process by enter time
+//because I can't get qsort to work
+void selection_sort(struct process *p[]) {
+	//temporary variable to hold 
+	struct process *temp;
+	int min;
+	
+	for(int i=0; i<num_processes; i++) {
+		min=i;
+		for(int j=i+1; j<num_processes; j++) {
+			if(p[j]->enter_time < p[min]->enter_time) {
+				min=j;
+			}
+		}
+		
+		//swap index i and min
+		
+		if(i != min) {
+			temp = p[i];
+			p[i] = p[min];
+			p[min] = temp;
+		}
+		
+		
+	}
+
+}
+
 
 
 
 /* FIRST IN FIRST OUT */
 
 
-void fifo(struct process p[]) {
+void fifo(struct process *p[]) {
 	int time_estimate = estimate_time(p);
 	
 	//make array to store process running data
@@ -296,77 +314,40 @@ void fifo(struct process p[]) {
 	for(int i=0; i<num_processes; i++) {
 		results_array[i] = calloc(time_estimate, sizeof(int));
 	}
-
+	
 	//sort processes by enter time
-	qsort(p, num_processes, sizeof(struct process), comp_times);
-
+	//qsort(p, num_processes, sizeof(struct process*), comp_times);
+	selection_sort(p);
 
 
 	//print_process_status(p, num_processes);
 
-
-	//keeps track of the total time of the system
-	//int master_time = 0;
 	
 	//keeps track of the process currently being run
 	int pc = 0;
+	int start_time = 0;
+	int end_time = time_estimate;
 
-	//TODO: this implementation does NOT handle time over
-	for(int t=0; t<time_estimate; t++) {
-		if(processes_completed(p) == 1) {
-			break;
-		}
+	pc = fifo_process(p, results_array, pc, start_time, end_time);
 	
-		//place newly entered processes in the ready list
-		int counter = pc;
-		while(p[counter].enter_time <= t) {
-			if(p[counter].status == 0) {
-				p[counter].status = 1;
-			}
-			counter ++;
-		}
-		
-		//set next process's status to running if it is running
-		if(p[pc].status == 1) {
-			p[pc].status = 3;
-		}
-		
-		//set process status to blocked if time for io
-		if(p[pc].status ==3 && is_io_time(p[pc])) {
-			p[pc].status =2;
-			p[pc].io_timer=0;
-		}
-		
-		//if io is finished, then set status to running again
-		if(p[pc].status == 2 && p[pc].io_timer == io_time) {
-			p[pc].status = 3;
-		}
-		
-		//if CPU time is over, then set status to finished and increment counter
-		if(p[pc].status == 3 && p[pc].time_counter == p[pc].CPU_time) {
-			p[pc].status = 4;
-			pc ++;
-			if(p[pc].status == 1) {
-				p[pc].status = 3;
-			}
-		}
-		
-		//increment time counters
-		if(p[pc].status == 3) {
-			p[pc].time_counter++;
-		}
-		
-		if(p[pc].status == 2) {
-			p[pc].io_timer ++;
-		}
-		
+	
+	//handles "time over"
+	while(processes_completed(p) == 0) {
+		printf("NOT FINISHED\n");
+		start_time += time_estimate;
+		end_time += time_estimate;
+			
+		//increase size of results array
 		for(int i=0; i<num_processes; i++) {
-			results_array[p[i].id][t] = p[i].status;
+			results_array[i] = realloc(results_array[i], sizeof(int)*end_time);
 		}
-	
+		pc = fifo_process(p, results_array, pc, start_time, end_time);
 	}
+		
 	
-	print_array_to_file(results_array, time_estimate, "fifo.csv");
+	
+	
+	print_array_to_file(results_array, pc, "fifo.csv");
 
 	printf("Made it out alive!\n");
 
@@ -375,8 +356,76 @@ void fifo(struct process p[]) {
 	for(int i=0; i<num_processes; i++) {
 		free(results_array[i]);
 		results_array[i] = NULL;
-	}
+	}  
 }
+
+
+int fifo_process(struct process *p[], int* results_array[], int pc, int time_start, int time_estimate) {
+	int t;
+	for(t=time_start; t<time_estimate; t++) {
+		if(processes_completed(p) == 1) {
+			break;
+		}
+		
+		//place newly entered processes in the ready list
+		int counter = pc;
+		
+		while(counter < num_processes && p[counter]->enter_time <= t) {
+		
+			if(p[counter]->status == 0) {
+				p[counter]->status = 1;
+			}
+			counter ++;
+		}
+		
+		//set next process's status to running if it is running
+		if(p[pc]->status == 1) {
+			p[pc]->status = 3;
+		}
+		
+		//set process status to blocked if time for io
+		if(p[pc]->status ==3 && is_io_time(p[pc])) {
+			p[pc]->status =2;
+			p[pc]->io_timer=0;
+		}
+		
+		//if io is finished, then set status to running again
+		if(p[pc]->status == 2 && p[pc]->io_timer == io_time) {
+			p[pc]->status = 3;
+		}
+		
+		//if CPU time is over, then set status to finished and increment counter
+		if(p[pc]->status == 3 && p[pc]->time_counter == p[pc]->CPU_time) {
+			p[pc]->status = 4;
+			pc ++;
+			if(p[pc]->status == 1) {
+				p[pc]->status = 3;
+			}
+		}
+		
+		//increment time counters
+		if(p[pc]->status == 3) {
+			p[pc]->time_counter++;
+		}
+		
+		if(p[pc]->status == 2) {
+			p[pc]->io_timer ++;
+		}
+		
+		for(int i=0; i<num_processes; i++) {
+			results_array[p[i]->id][t] = p[i]->status;
+		}
+		
+	}
+	
+	if(processes_completed(p) == 1) {
+		return t;
+	} else {
+		return pc;
+	}
+
+}
+
 
 
 void print_array_to_file(int* array[], int l, char* file_name) {
@@ -396,9 +445,9 @@ void print_array_to_file(int* array[], int l, char* file_name) {
 
 
 
-int is_io_time(struct process x) {
-	for(int i=0; i<x.num_io; i++) {
-		if(x.time_counter == x.io_times[i]) {
+int is_io_time(struct process *x) {
+	for(int i=0; i<x->num_io; i++) {
+		if(x->time_counter == x->io_times[i]) {
 			return 1;
 		}
 	}
@@ -406,9 +455,9 @@ int is_io_time(struct process x) {
 }
 
 
-int processes_completed(struct process p[]) {
+int processes_completed(struct process *p[]) {
 	for(int i=0; i<num_processes; i++) {
-		if(p[i].time_counter < p[i].CPU_time) {
+		if(p[i]->time_counter < p[i]->CPU_time) {
 			return 0;
 		}
 	}
@@ -420,12 +469,12 @@ int comp_times(const void *a, const void *b) {
 	return (*(struct process*)a).enter_time - (*(struct process*)b).enter_time; 
 }
 
-int estimate_time(struct process p[]) {
+int estimate_time(struct process *p[]) {
 	int time_estimate = 0;
 	
 	for(int i=0; i<num_processes; i++) {
-		time_estimate += p[i].CPU_time;
-		time_estimate += p[i].num_io * io_time;
+		time_estimate += p[i]->CPU_time;
+		time_estimate += p[i]->num_io * io_time;
 	}
 	
 	return time_estimate;
@@ -433,20 +482,23 @@ int estimate_time(struct process p[]) {
 
 
 
-void print_process_status(struct process p[], int np) {
+void print_process_status(struct process *p[], int np) {
   for(int i=0; i<np; i++) {
-    printf("Process %d:\n", p[i].id);
-    printf("Enters system at %d\n", p[i].enter_time);
-    printf("Total CPU time: %d\n", p[i].CPU_time);
-    printf("Priority: %d\n", p[i].priority);
+    printf("Process %d:\n", p[i]->id);
+    
+    
+    printf("Enters system at %d\n", p[i]->enter_time);
+    printf("Total CPU time: %d\n", p[i]->CPU_time);
+    printf("Priority: %d\n", p[i]->priority);
+   
    
     printf("I/O request times: ");
-    for(int j=0; j<p[i].num_io; j++) {
-      printf("%d ", p[i].io_times[j]);   
+    for(int j=0; j<p[i]->num_io; j++) {
+      printf("%d ", p[i]->io_times[j]);   
     }
     printf("\n");
     
-    printf("CPU time completed: %d\n", p[i].time_counter);
-    printf("\n\n");
+    printf("CPU time completed: %d\n", p[i]->time_counter);
+    printf("\n\n"); 
   }
 }
